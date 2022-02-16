4f2ec26a57af79dd1e5fe86f0c6f74f4c7c8aab2
woozink <k0109974@naver.com>
2022-02-16 18:15:53 +0900

    Spring Bean Ex

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -2,9 +2,12 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
